cf48b1662e1b24af75f01b518d80f57e2faa2b7a
Ahmed55524 <ahmed.dhouib1202@gmail.com>
2023-02-07 09:27:21 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import React, { Component } from "react";
 import './App.css';
+export default class App extends Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+state = {
+  counter : 0,
+  isVisible : true,
+};
+
+  inc =()=> {this.setState({counter : this.state.counter+1})}
+  dec =()=> {
+    if (this.state.counter>0) {
+    {this.setState({counter : this.state.counter-1})}
+    }
+  };
+  handleShow =()=> {
+    this.setState({isVisible: !this.state.isVisible})
+  };
+  render() {
+    return (
+      
+      <div className='App'>
+        <div>
+        <button onClick={() => this.handleShow()}> {this.state.isVisible ? "Hide":"Show"}</button>
+        <h1>Counter : {this.state.counter}</h1>
+        <button onClick={() => this.inc()}>+</button>
+        <button onClick={()=> this.dec()}>-</button>
+        </div>
+      </div>
+      )
+  }
 }
-
-export default App;
